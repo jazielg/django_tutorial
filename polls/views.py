8c970fba87731a9ba1abb70ac356c9f722ebf554
jazielg <jazielgj1995@gmail.com>
2018-11-28 20:19:02 -0200

    commit inicial

---
+++ polls/views.py
@@ -0,0 +1,67 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.template import loader
+from django.views import generic
+from django.utils import timezone
+
+from .models import Question, Choice
+
+
+class IndexView(generic.ListView):
+	template_name = 'polls/index.html'
+	context_object_name = 'latest_question_list'
+
+	def get_queryset(self):
+		# Retorna os ultimos 5 dados da tabela Question
+		# return Question.objects.order_by('-pub_date')[:5]
+		# lte = less than or equal(<=)
+		return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date').exclude(choice__question__isnull=True)[:5]
+
+
+class DetailView(generic.DetailView):
+	model = Question
+	template_name = 'polls/detail.html'
+
+	def get_queryset(self):
+		return Question.objects.filter(pub_date__lte=timezone.now())
+
+
+class ResultsView(generic.DetailView):
+	model = Question
+	template_name = 'polls/results.html'
+
+	def get_queryset(self):
+		return Question.objects.filter(pub_date__lte=timezone.now())
+
+
+def vote(request, question_id):
+	question = get_object_or_404(Question, pk=question_id)
+	try:
+		selected_choice = question.choice_set.get(pk=request.POST['choice'])
+	except (KeyError, Choice.DoesNotExist):
+		return render(request, 'polls/detail.html', {
+			'question': question,
+			'error_message': 'You did not select a choice',
+		})
+	else:
+		selected_choice.votes += 1
+		selected_choice.save()
+		return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+# 	latest_question_list = Question.objects.order_by('-pub_date')[:5]
+# 	template = loader.get_template('polls/index.html')
+# 	context = {
+# 		'latest_question_list': latest_question_list,
+# 	}
+# 	return render(request, 'polls/index.html', context)
+# 	#output = ', '.join([q.question_text for q in latest_question_list])
+# 	#return HttpResponse(template.render(context, request))
+
+# def detail(request, question_id):
+# 	question = get_object_or_404(Question, pk=question_id)
+# 	return render(request, 'polls/detail.html', {'question':question})
+
+# def results(request, question_id):
+# 	question = get_object_or_404(Question, pk=question_id)
+# 	return render(request, 'polls/results.html', {'question':question})
